9e1b1ecf98e97a10b6636e3cc910294aa84d83f7
aditya <dev@chimeracoder.net>
2015-03-09 15:09:54 -0400

    Add (failing) test for parsing nested JSON

--- parser_test.go
+++ parser_test.go
@@ -1,10 +1,18 @@
 package main
 
 import "testing"
-
+import "log"
 
 func Test_ParseSimpleJSON(t *testing.T) {
-    if err := parse(SimpleJSON); err != nil{
-        t.Error(err)
-    }
+	log.Printf("parse simple")
+	if err := parse(SimpleJSON); err != nil {
+		t.Error(err)
+	}
+}
+
+func Test_ParseNestedJSON(t *testing.T) {
+	log.Printf("parse nested")
+	if err := parse(NestedJSON); err != nil {
+		t.Error(err)
+	}
 }
