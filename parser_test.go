0df39c8c49ecac03ee49ea5d06d4834f1de958ef
aditya <dev@chimeracoder.net>
2015-03-10 12:22:39 -0400

    Add test for parsing nested JSON and ensuring correct AST

--- parser_test.go
+++ parser_test.go
@@ -6,17 +6,31 @@ import (
 )
 
 func Test_ParseSimpleJSON(t *testing.T) {
+	expected := map[string]interface{}{"a": 5}
+
 	ast, err := parse(SimpleJSON)
 	if err != nil {
 		t.Error(err)
 	}
-	expected := map[string]interface{}{"a": 5}
 	if !reflect.DeepEqual(ast, expected) {
 		t.Errorf("AST does not match: %+v, %+v", ast, expected)
 	}
 }
 
 func Test_ParseNestedJSON(t *testing.T) {
+	expected := map[string]interface{}{"a": 5, "b": "bar", "cat": map[string]interface{}{"dog": true, "elephant": []interface{}{"hathi", 3}}}
+
+	ast, err := parse(NestedJSON)
+	if err != nil {
+		t.Error(err)
+	}
+	if !reflect.DeepEqual(ast, expected) {
+		t.Errorf("AST does not match: %+v, %+v", ast, expected)
+	}
+
+}
+
+func Test_ParseJSFunction(t *testing.T) {
 	if _, err := parse(NestedJSON); err != nil {
 		t.Error(err)
 	}
